679f17c8b15814cc12cd0afd274ef35714d03657
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-30 14:17:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,14 +7,27 @@ class Song
   @@genres = []
   @@genre_count = {}
   
-  def initialize
-    @@count+=1
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist if !(@@artists.include? artist)
+    @@artist_count += 1
+    @@genres << genre if !(@@genres.include? genre)
+    @@genre_count += 1
   end
   
   def self.count
     @@count
   end
   
+  def self.artists
+    @@artists
+  end
   
+  def self.genres
+    @@genres
+  end
   
 end
